1af8f4df3d2d8c0dd16247b0cc72a7bd657c8b53
Omar Silva <osilva@almightyco.com>
2017-01-31 20:16:28 -0400

    added site

--- directions/script.js
+++ directions/script.js
@@ -1,131 +1,233 @@
-/*global alert */
-/*jslint browser:true, white: true, debug: true */
+/*jslint browser:true, white: true, debug: true, regexp: true */
 
+/**
+ * Get the type of icon for each node in the result list based on the selected
+ * travel method and vehicle type.
+ * @param {xmlDoc} step - XML node that includes all the information for a single step.
+ * @return {string} HTML-formatted string with the icon to be used in the result list.
+ */
 function getIconType(step) {
     "use strict";
+    var travelMode = step.getElementsByTagName("travel_mode")[0]
+        .childNodes[0].nodeValue,
+        vehicleType;
     
-    if (step.travel_mode === "TRANSIT") {
-        switch(step.transit_details.line.vehicle.type) {
+    if (travelMode === "TRANSIT") {
+        
+        vehicleType = step.getElementsByTagName("transit_details")[0].getElementsByTagName("line")[0]
+        .getElementsByTagName("vehicle")[0].getElementsByTagName("type")[0].childNodes[0].nodeValue;
+        
+        switch(vehicleType) {
             case "BUS":
-                return "<i class='material-icons icon'>directions_bus</i>";
+                return "<i class='material-icons icon black'>directions_bus</i>";
             case "SUBWAY":
-                return "<i class='material-icons icon'>directions_subway</i>";
+                return "<i class='material-icons icon black'>directions_subway</i>";
             case "RAILWAY":
-                return "<i class='material-icons icon'>directions_railway</i>";
+                return "<i class='material-icons icon black'>directions_railway</i>";
             case "BOAT":
-                return "<i class='material-icons icon'>directions_boat</i>";
+                return "<i class='material-icons icon black'>directions_boat</i>";
             default:
-                return "<i class='material-icons icon'>directions_transit</i>";
+                return "<i class='material-icons icon black'>directions_transit</i>";
         }
     }
-    else if (step.travel_mode === "WALKING") {
-        return "<i class='material-icons icon'>directions_walk</i>";     
+    else if (travelMode === "WALKING") {
+        return "<i class='material-icons icon black'>directions_walk</i>";
+    }
+    else if (travelMode === "BICYCLING") {
+        return "<i class='material-icons icon black'>directions_bike</i>";
+    }
+    else {
+        return "<i class='material-icons icon black'>directions_car</i>";
     }
 }
 
+/**
+ * Returns an HTML-formatted string with the information to be displayed for a 
+ * single 'step' in the direction list.
+ * @param {xmlDoc} step - XML node that includes all the information for a single step.
+ * @return {string} HTML-formatted string with the information to be displayed.
+ */
 function getStepInfo(step) {
     
     "use strict";
     var s = "",
-        duration = step.duration.value;
-    
-    if (step.travel_mode === "TRANSIT") {
+        regex = /(<([^>]+)>)/ig,
+        durationValue = step.getElementsByTagName("duration")[0]
+        .getElementsByTagName("value")[0].childNodes[0].nodeValue,
         
-        if (typeof step.transit_details.line.short_name !== 'undefined') {
+        durationText = step.getElementsByTagName("duration")[0]
+        .getElementsByTagName("text")[0].childNodes[0].nodeValue,
+        
+        distanceText = step.getElementsByTagName("distance")[0]
+        .getElementsByTagName("text")[0].childNodes[0].nodeValue,
+        
+        htmlInstructions = step.getElementsByTagName("html_instructions")[0].childNodes[0].nodeValue,
+        travelMode = step.getElementsByTagName("travel_mode")[0].childNodes[0].nodeValue,
+        transitShortName;
+    
+    if (travelMode === "TRANSIT") {
+        
+        transitShortName  = step.getElementsByTagName("short_name")[0];
+        
+        if (typeof transitShortName !== 'undefined') {
             s += "<span class='bg-primary bus-route'>" 
-            + step.transit_details.line.short_name
+            + transitShortName.childNodes[0].nodeValue
             + "</span>";
         }
     }
     
-    s += step.html_instructions;
+    s += htmlInstructions.replace(regex, " ");
     
-    if (Math.floor(step.duration.value) > 0) {
-        s += " (about " + step.duration.text + ")";
+    if (Math.floor(durationValue) > 0) {
+        s += " (about " + durationText + ")";
     }
     
-    s += " ~ " + step.distance.text;
+    s += " ~ " + distanceText;
     
     return s;
 }
 
-function displayDirections(JSONObj) {
+function displayDirections(xmlDoc) {
     "use strict";
     var resultArea = document.getElementById("results"),
-        route = JSONObj.routes[0],
-        leg = route.legs[0],
-        steps = leg.steps,
+        leg = xmlDoc.getElementsByTagName("route")[0].getElementsByTagName("leg")[0],
+        startAddress = leg.getElementsByTagName("start_address")[0].childNodes[0].nodeValue,
+        endAddress = leg.getElementsByTagName("end_address")[0].childNodes[0].nodeValue,
+        totalDistance = leg.getElementsByTagName("distance")[leg.getElementsByTagName("distance").length - 1].querySelector("text").childNodes[0].nodeValue,
+        totalDuration = leg.getElementsByTagName("duration")[leg.getElementsByTagName("duration").length - 1].querySelector("text").childNodes[0].nodeValue,      
+        steps = xmlDoc.getElementsByTagName("step"),
         step,
         i,
         htmlString = "";
     
+    htmlString += "<dl class='row'>";
+    htmlString += "<dt class='col-sm-4'>Total Travel Time: </dt>";
+    htmlString += "<dd class='col-sm-8'>" + totalDuration + "</dd>";
+    htmlString += "<dt class='col-sm-4'>Total Travel Distance: </dt>";
+    htmlString += "<dd class='col-sm-8'>" + totalDistance + "</dd>";
+    htmlString += "</dl>";
     htmlString += "<div>";
     htmlString += "<ul class='list-group'>";
-    htmlString += "<li class='list-group-item'><i class='material-icons icon'>place</i>" + leg.start_address + "</li>";
+    htmlString += "<li class='list-group-item'><i class='material-icons icon red-icon'>place</i>" 
+        + startAddress + "</li>";
     
     for (i = 0; i < steps.length; i = i + 1) {
         step = steps[i];
         
-        htmlString += "<li class='list-group-item'>" + getIconType(step) 
-        + getStepInfo(step) + "</li>";
+        if (typeof step.getElementsByTagName("html_instructions")[0] !== 'undefined') {
+            htmlString += "<li class='list-group-item'>" + getIconType(step) 
+            + getStepInfo(step) + "</li>";
+        }
     }
     
-    htmlString += "<li class='list-group-item'><i class='material-icons icon'>place</i>" + leg.end_address + "</li>";
+    htmlString += "<li class='list-group-item'><i class='material-icons icon red-icon'>place</i>" 
+        + endAddress + "</li>";
     htmlString += "</ul>";
     htmlString += "</div>";
     
     htmlString += "<div class='margin-top-24px'>";
-    htmlString += " <dl class='dl-horizontal'>";
-    htmlString += "     <dt>Total Travel Time</dt>";
-    htmlString += "     <dd>" + leg.duration.text + "</dd>";
-    htmlString += "     <dt>Total Travel Distance</dt>";
-    htmlString += "     <dd>" + leg.distance.text + "</dd>";
-    htmlString += " </dl>";
     htmlString += "</div>";
+
     
     resultArea.innerHTML = htmlString;
+}
+
+function displayAlert(string) {
+    "use strict";
+    var results = document.getElementById("results");
+    
+    results.innerHTML = "<div class='alert alert-danger'>" + string + "</div>";
 }
 
 function loadData(response) {
     
     "use strict";
-    var JSONText = response.responseText,
-        JSONObj;
+    var xmlDoc = response.responseXML;
     
-    if (JSONText !== null) {
-        JSONObj = JSON.parse(JSONText);
-        displayDirections(JSONObj);
+    if (xmlDoc) {
+        
+        if (xmlDoc.getElementsByTagName("status")[0].childNodes[0].nodeValue === "OK" ) {
+            displayDirections(xmlDoc);
+        }
+        else {
+            displayAlert("No results found. Change your parameters and try again.");
+        }
     }
+}
+
+function doValidation() {
+    "use strict";
+    var originInput = document.getElementById("origin"),
+        originGroup = document.getElementById("origin-group"),
+        originFb = document.getElementById("origin-feedback"),
+        destInput = document.getElementById("dest"),
+        destGroup = document.getElementById("dest-group"),
+        destFb = document.getElementById("dest-feedback"),
+        isValid = true;
+    
+    if (originInput.value === "") {
+        originGroup.className += " has-danger";
+        originInput.className += " form-control-danger";
+        originFb.innerHTML = "Origin must not be blank!";
+        isValid = false;
+    }
+    else {
+        originGroup.className = "form-group";
+        originInput.className = "form-control";
+        originFb.innerHTML = "";
+    }
+    
+    if (destInput.value === "") {
+        destGroup.className += " has-danger";
+        destInput.className += " form-control-danger";
+        destFb.innerHTML = "Destination must not be blank!";
+        isValid = false;
+    }
+    else {
+        destGroup.className = "form-group";
+        destInput.className = "form-control";
+        destFb.innerHTML = "";
+    }
+    
+    return isValid;
 }
 
 function showDirections() {
     
     "use strict";
     var results = document.getElementById("results"),
+        modeSelect = document.getElementById("mode"),
+        mode = modeSelect.options[modeSelect.selectedIndex].value,
         origin = document.getElementById("origin").value,
         dest = document.getElementById("dest").value,
         time = document.getElementById("arrival").value,
         date = (time) ? new Date(time) : new Date(),
         apiKey = "AIzaSyBfI_SFoa5_M9Qb_7ZaotXzd2JUvDSWbNM",
-        restCall = "https://maps.googleapis.com/maps/api/directions/json?origin=" 
+        restCall = "https://maps.googleapis.com/maps/api/directions/xml?transit_routing_preference=less_walking&origin=" 
         + encodeURI(origin) + 
         "&destination=" 
         + encodeURI(dest) + 
         "&arrival_time="
         + Math.round(date.getTime() / 1000) +
-        "&mode=transit&key=" 
+        "&mode=" + mode + "&key=" 
         + apiKey,
-        
         xhttp = new XMLHttpRequest();
+    
+    if (!doValidation()) {
+        return;
+    }
     
     xhttp.onload = function () {
         if (this.status === 200) {
             loadData(this);
         }
+        else if (this.status === 400) {
+            displayAlert("Request Status 400: invalid syntax");
+        }
     };
     
     xhttp.onerror = function (err) {
-        results.innerHTML = "<div class='alert alert-danger'>Error processing request. Try again later.</div>";
+        displayAlert("Error processing request: " + err);
     };
     
     results.style.visibility = "visible";
@@ -133,5 +235,14 @@ function showDirections() {
     
     xhttp.open("GET", restCall, true);
     xhttp.send();
+}
+
+function clearFields() {
+    "use strict";
+    document.getElementById("origin").value = "";
+    document.getElementById("dest").value = "";
+    document.getElementById("arrival").value = "";
+    document.getElementById("results").innerHTML = "";
+    document.getElementById("results").style.visibility = "hidden";
 }
 
