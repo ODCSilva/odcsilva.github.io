7c4417861919e26aa26cfaee85833483ca59c40a
Omar Silva <osilva@almightyco.com>
2017-02-19 21:07:17 -0400

    working

--- note2/script.js
+++ note2/script.js
@@ -2,6 +2,22 @@ $(document).ready(function () {
 
   fitContent();
 
+  $("#auth").modal();
+
+  $("#auth-button").click(function () {
+    var client_id = "64a0b705-0799-4ae6-a8c3-2203dd6b38e5",
+        redirect_uri = "http://omarsilva.net/note2/",
+        scope = "office.onenote_update wl.signin",
+        url = "https://login.live.com/oauth20_authorize.srf?response_type=code&client_id="
+        + client_id +
+        "&redirect_uri=" + redirect_uri +
+        "&scope=" + scope;
+
+        $.get(url, function (data) {
+          alert(data);
+        });
+  });
+
   function fitContent() {
     $("#content").css("top", $("#mainNav").css("height"));
     $("#content").outerHeight($(document).height() - $("#mainNav").outerHeight());
