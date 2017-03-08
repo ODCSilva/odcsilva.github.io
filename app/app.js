456e3187c37936be154c0bd2df8d6ebdd5f0b1c2
Omar Silva <osilva@almightyco.com>
2017-03-07 21:17:17 -0400

    made more changes

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-var app = angular.module("MyPortfolio", ['ngMaterial'])
-.config(function($mdThemingProvider) {
+var app = angular.module("MyPortfolio", ['ngMaterial','ngRoute','ngAnimate'])
+.config(function($mdThemingProvider, $routeProvider) {
 
 	$mdThemingProvider.theme('default')
 	.primaryPalette('orange')
@@ -8,6 +8,23 @@ var app = angular.module("MyPortfolio", ['ngMaterial'])
 		'default':'900'
 	})
 	.dark();
+
+	$routeProvider
+	.when('/', {
+		selectedIndex: 0
+	})
+	.when('/skills', {
+		selectedIndex: 1
+	})
+	.when('/projects', {
+		selectedIndex: 1
+	})
+	.when('/resume', {
+		selectedIndex: 1
+	})
+	.when('/contact', {
+		selectedIndex: 1
+	})
 })
 .controller("SectionController", function($scope) {
 	$scope.sections = [
@@ -85,38 +102,59 @@ var app = angular.module("MyPortfolio", ['ngMaterial'])
 		$mdSidenav('right').toggle();
 	};
 })
-.controller("TabController", function($scope) {
+.controller("TabController", function($scope, $location) {
+	$scope.selectedIndex = 0;
+
+	$scope.$watch('selectedIndex', function(newIndex, oldIndex) {
+		switch(newIndex) {
+			case 0:
+				$location.url('/');
+				break;
+			case 1:
+				$location.url('/skills');
+				break;
+			case 2:
+				$location.url('/projects');
+				break;
+			case 3:
+				$location.url('/resume');
+				break;
+			case 4:
+				$location.url('/contact');
+				break;
+		}
+	});
 
 	$scope.sections = [
 		{
 			name: "About",
 			icon_name: "fa-user",
 			icon_set: "fa",
-			src: "about.html"
+			src: "views/about.html"
 		},
 		{
 			name: "Programming Languages & Skills",
 			icon_name: "fa-code",
 			icon_set: "fa",
-			src: "languages.html"
+			src: "views/languages.html"
 		},
 		{
 			name: "Projects",
 			icon_name: "fa-cogs",
 			icon_set: "fa",
-			src: "project.html"
+			src: "views/project.html"
 		},
 		{
 			name: "Resume",
 			icon_name: "fa-file-text-o ",
 			icon_set: "fa",
-			src: "languages.html"
+			src: "views/languages.html"
 		},
 		{
 			name: "Contact",
 			icon_name: "fa-address-card-o",
 			icon_set: "fa",
-		    src: "contact.html"
+		    src: "views/contact.html"
 		}
 	];
 })
