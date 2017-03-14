9176e29b79443ad9b95e3085eb47de6570665d62
Omar Silva <osilva@almightyco.com>
2017-03-13 23:19:46 -0300

    Added more projects

--- app/app.js
+++ app/app.js
@@ -345,6 +345,65 @@ function ($scope, $mdDialog, $mdMedia, LanguageService) {
 			],
 			github: "https://github.com/ODCSilva/Note2",
 			demo: "demos/note2/"
+		},
+		{
+			name: "KinSHIFT",
+			description: "KinSHIFT is a fitness-oriented social media app for Android that I worked on with Almighty Software Company.",
+			languages: [LanguageService.java],
+			platforms: "Android",
+			screenshots: [
+				{ src: "assets/img/projects/kinSHIFT/1.jpg"},
+				{ src: "assets/img/projects/kinSHIFT/2.jpg"},
+				{ src: "assets/img/projects/kinSHIFT/3.jpg"}
+			]
+		},
+		{
+			name: "Zombie Sim",
+			description: "Conway's Game of Life meets the zombie apocalypse in this console-based zombie vs human simulation.",
+			languages: [LanguageService.cpp],
+			platforms: "Windows",
+			screenshots: [
+				{ src: "assets/img/projects/zombiesim/1.jpg"},
+				{ src: "assets/img/projects/zombiesim/2.jpg"},
+			],
+			github: "https://github.com/ODCSilva/Zombie-Sim"
+		},
+		{
+			name: "Line Editor",
+			description: "A console-based line editor that uses linked lists to "
+			+ "read and store a file buffer. Changes made are store in an output file specified in the command-line arguments.",
+			languages: [LanguageService.cpp],
+			platforms: "Windows",
+			screenshots: [
+				{ src: "assets/img/projects/lineeditor/1.jpg"},
+				{ src: "assets/img/projects/lineeditor/2.jpg"}
+			],
+			github: "https://github.com/ODCSilva/Line-Editor/"
+		},
+		{
+			name: "Commute Planner",
+			description: "A web app that uses Google Map's direction API to calculate steps, commute time, and distance between two points. Uses XML as response data.",
+			languages: [LanguageService.html5, LanguageService.js],
+			platforms: "Web",
+			screenshots: [
+				{ src: "assets/img/projects/commute-planner/1.jpg"},
+				{ src: "assets/img/projects/commute-planner/2.jpg"}
+			],
+			github: "https://github.com/ODCSilva/Commute-Planner/"
+		},
+		{
+			name: "Star Trivia",
+			description: "I built this Android app as part of an assigment to genrate quizzes out of a text file. Wrong answers needed to be randomized, as well as the order they were presented in. The question order was also ranzomized.",
+			languages: [LanguageService.java],
+			platforms: "Android",
+			screenshots: [
+				{ src: "assets/img/projects/star-trivia/1.jpg"},
+				{ src: "assets/img/projects/star-trivia/2.jpg"},
+				{ src: "assets/img/projects/star-trivia/3.jpg"},
+				{ src: "assets/img/projects/star-trivia/4.jpg"},
+				{ src: "assets/img/projects/star-trivia/5.jpg"}
+			],
+			github: "https://github.com/ODCSilva/StarTrivia/"
 		}
 	];
 
